8b5894347f6309ddb3f85e733bf93724d567fa14
大魔王 <492843984@qq.com>
2017-02-20 16:26:09 +0800

    Ukit

---
+++ UkitMotor.h
@@ -0,0 +1,60 @@
+#ifndef _UKITMOTOR_H__
+#define _UKITMOTOR_H__
+
+//导入Arduino核心头文件
+#include"Arduino.h"  
+#define SERVO_NUMER_MAX		16	//支持的最多舵机
+//置位变量的某位
+#define  Clr_bit(val, bit)  (val &= ~(1 << bit))
+//清位变量的某位
+#define  Set_bit(val, bit)  (val |= (1 << bit))
+//读变量的某位
+#define RXD_OFFSET  10  //接收消息偏移
+//#define  Read_bit(val, bit)  (val & (1 << bit))  //结果拿去计算有问题
+#define  Read_bit(val, bit)  ((val & (1 << bit)) ? 1 : 0)
+#ifndef NULL
+#define  NULL         ((void *) 0)
+#endif
+
+
+
+#ifndef swab16
+#define swab16(x) ((x&0x00ff) << 8 | (x&0xff00) >> 8)
+#endif
+
+#ifndef swab8
+#define swab8(x) ((x&0x0f) << 4 | (x&0xf0) >> 4)
+#endif
+class UkitMotor
+{
+private:
+	typedef	struct _ACTION_PACK_Struct_	//结构顺序不可调
+{
+	unsigned  char Angle[SERVO_NUMER_MAX];	//
+	unsigned  short Run_time16;	//运行时间,分辨率ms
+}ACTION_PACK;
+typedef  struct _ANGLE_PACK_V002_Struct_ //结构顺序不可调,可在后面添加 V0.02
+{
+  unsigned  char Angle[SERVO_NUMER_MAX];  //1~20舵机角度
+  unsigned  short Run_time16; //运行时间,分辨率ms
+}ANGLE_PACK_V002;
+  unsigned char gServos = SERVO_NUMER_MAX;  //舵机个数
+  unsigned char gServo_list[SERVO_NUMER_MAX / 8]; //舵机列表,位表示
+  unsigned char Usart3_Rx_Buf[64];
+  unsigned char Usart3_Rx_Buf_count=0;
+  volatile ANGLE_PACK_V002 gsSave_Angle;    //存角度,相同角度不再下发
+	
+public:
+	UkitMotor();
+	~UkitMotor();
+	unsigned char Cheak_Sum(unsigned char len, unsigned char *buf);
+	unsigned long Servo_Tx(unsigned char Head,unsigned char ServoNO,unsigned char len,unsigned char CMD,unsigned char * Data);
+	unsigned char Get_Servo_list(unsigned char Servo_NO);  //检查舵机存在不 ? 正不正常
+	void Set_Servo_list(unsigned char Servo_NO);
+	void Clr_Servo_list(unsigned char Servo_NO);
+	void check_servo();
+  void motor(char ID,int dir, int speed);
+  void servo(char ID,int angle, int time);
+
+};	
+#endif
---
+++ motor.ino
@@ -0,0 +1,224 @@
+#include"UkitMotor.h"
+#include "SR04.h"
+#define TRIG_PIN 30
+#define ECHO_PIN 32
+UkitMotor motor;
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+
+bool Rdec;
+bool Ldec;
+int Find;
+int num1,num2,num3,num4,num5;
+unsigned char tData[32] ;
+long a;
+long b;
+long c;
+void setup() {
+    // put your setup code here, to run once:
+  Serial.setTimeout(2);  //设置超时ms
+  Serial.begin(115200);
+  delay(1000);  //开机延时
+  motor.check_servo();  //获取舵机个数,列表
+  //motor.Servo_Tx(0xFA,12,4,0x01,120<<0 | 200<<8 );
+
+  delay(2);
+  Serial.print(Ldec,Rdec);
+  delay(100);
+        tData[0] = 120;
+        tData[1] = (100/20);
+  motor.Servo_Tx(0xFA,12,4,0x01,tData);
+
+}
+
+void loop() {
+  //turnL(765);
+  Tandtenfindline(765,0);
+// findline(765);
+
+}
+void Tandtenfindline(int speed,int dir)
+{
+  if(dir==0)
+  {
+  findline(speed);
+  if((num1==0)&&(num5==0))
+  { 
+    forward(speed);
+    delay(200);
+    turnL(speed);
+    delay(1000);
+   // stop();
+    while(1){
+        num3=digitalRead(11);  //循环判断中间传感器度数，
+        if(num3==1)                                                                                                                                                                                                                                                            
+        {  
+          turnL(speed);
+          delay(200);//如果num2==1说明还没有转到中间位置，继续左转
+        }
+        else
+          break; 
+          }   
+  
+  }
+  }
+    if(dir==1)
+  {
+  findline(speed);
+  if((num1==0)&&(num5==0))
+  { 
+    forward(speed);
+    delay(200);
+    turnR(speed);
+    delay(1000);
+   // stop();
+    while(1){
+        num3=digitalRead(11);  //循环判断中间传感器度数，
+        if(num3==1)                                                                                                                                                                                                                                                            
+        {  
+          turnR(speed);
+          delay(200);//如果num2==1说明还没有转到中间位置，继续左转
+        }
+        else
+          break; 
+          }   
+  
+  }
+  }
+}
+void bizhang()
+{
+  if(a<10 and Ldec==0 and Rdec==0)
+{
+  stop();
+  delay(100);
+  tData[0] = 30;
+  tData[1] = (100/20);
+  motor.Servo_Tx(0xFA,12,4,0x01,tData);
+  delay(1000);
+  b=sr04.Distance();
+  delay(10);
+  if(b<10)
+    Ldec=1;
+  delay(1100);  
+  tData[0] = 215;
+  tData[1] = (100/20);
+  motor.Servo_Tx(0xFA,12,4,0x01,tData);
+  delay(1000);
+  c=sr04.Distance();
+  delay(20);
+   if(c<10)
+    Rdec=1;
+  tData[0] = 120;
+  tData[1] = (100/20);
+  motor.Servo_Tx(0xFA,12,4,0x01,tData);
+  delay(1000);
+}
+else if(Ldec==1 and Rdec==0)
+{
+  turnR(760);
+  delay(2000);
+  forward(0);
+  //Ldec=0;
+}
+else if(Rdec==1 and Ldec==0)
+{
+  turnL(760);
+  delay(2000);
+  forward(0);
+  //Rdec=0;
+}
+else if(Rdec==1 and Ldec==1)
+{
+  back(760);
+  delay(2000);
+ // Ldec=0;
+//  Rdec=0;
+}
+else 
+{
+  forward(760);
+
+}
+ 
+}
+void findline(int i)
+{
+
+  num1=digitalRead(31);    
+  num2=digitalRead(33);
+  num3=digitalRead(35);
+  num4=digitalRead(37);
+  num5=digitalRead(39);   
+ 
+  if((num2==0)&&num3&&num4) 
+   {
+      turnL(i);          //检测到左边传感器遇到黑线，说明小车右偏了，立即往左转一点
+      delay(40);
+      while(1){
+        num3=digitalRead(11);  //循环判断中间传感器度数，
+        if(num3==1)
+        {  
+          turnL(i);
+          delay(200);//如果num2==1说明还没有转到中间位置，继续左转
+        }
+        else
+          break; 
+       
+          }                                      
+   }    
+    
+  else if(num3&&num2&&(num4==0))
+   {
+      turnR(i);
+      delay(40);
+      while(1)
+      {
+        num3=digitalRead(11);
+        if(num3==1)
+        {
+           turnR(i);  
+           delay(120); 
+
+        }
+        else
+            break;
+        
+      }
+   }
+  
+  else
+  {
+    forward(i); 
+    delay(2);
+  } 
+  }
+void forward(int speed)
+{
+  motor.motor(4,0,speed);
+  motor.motor(2,1,speed);
+}
+void turnL(int speed)
+{
+  motor.motor(4,1,speed);
+  motor.motor(2,1,speed);
+  
+
+}
+void turnR(int speed)
+{
+
+  motor.motor(2,0,speed);
+  motor.motor(4,0,speed);
+
+}
+void back(int speed)
+{
+  motor.motor(4,1,speed);
+  motor.motor(2,0,speed);
+}
+void stop()
+{
+  motor.motor(4,0,0);
+  motor.motor(2,0,0);
+}
+
